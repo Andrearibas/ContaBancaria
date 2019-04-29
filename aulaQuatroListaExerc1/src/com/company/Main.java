8c03ed9da42921d6fde84f0573f70013ba5300be
Andrearibas <deia888@hotmail.com>
2019-04-29 12:04:20 -0300

    Inclusao atividade

---
+++ aulaQuatroListaExerc1/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int umNumero = 6;
+        boolean umNum = umNumero /2;
+
+        if (umNum % 2){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+
+    }
+}
